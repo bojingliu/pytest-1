bd19beaf7c5f5d4d8b3b7191546dfad9a1348f61
bojingliu <liubojings@gmail.com>
2019-03-27 14:20:03 +0100

    should have linked to travis first

--- example.py
+++ example.py
@@ -10,7 +10,7 @@ def test_add():
 def subtract(a, b):
     return a + b  # <--- fix this in step 8
 
-
+# should have linked to travis first
 # uncomment the following test in step 5
 #def test_subtract():
 #    assert subtract(2, 3) == -1
